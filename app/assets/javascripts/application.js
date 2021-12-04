003869f4736049f75e807b7e69d6eb67fb40e888
KentaEbisui <kenta.e.1118@gmail.com>
2021-12-04 05:10:39 +0000

    デプロイ準備

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,14 +18,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-const CLASSNAME = "-visible";
-const TIMEOUT = 1500;
-const $target = $(".title");
-
-setInterval(() => {
-  $target.addClass(CLASSNAME);
-  setTimeout(() => {
-    $target.removeClass(CLASSNAME);
-  }, TIMEOUT);
-}, TIMEOUT * 2);
